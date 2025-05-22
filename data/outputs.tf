45c21fd7a8ebf6d9e560d93f679b7f6fc0d72b24
sultonazarmamadazizov <sultonazar.mamadazizov@mail.ru>
2025-05-22 22:58:23 +0100

    Add output for load balancer public security group
    This change introduces a new output, `lb_public_security_group`, to expose the load balancer's public security group from the data module. It ensures that necessary security group details are easily accessible for dependent modules or outputs.

--- data/outputs.tf
+++ data/outputs.tf
@@ -6,6 +6,10 @@ output "redis_security_group" {
     value = data.aws_security_group.redis_security_group
 }
 
+output "lb_public_security_group" {
+    value = data.aws_security_group.lb_public_security_group
+}
+
 output "elasticache_subnet_group" {
     value = data.aws_elasticache_subnet_group.elasticache_subnet_group
 }
