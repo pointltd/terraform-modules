9ac2af29b92a4d6db6ca677ad9749972e8728ad3
sultonazarmamadazizov <sultonazar.mamadazizov@mail.ru>
2025-05-23 18:39:12 +0100

    Rename resource block for consistency in naming.
    Updated the `aws_alb` resource block name from `alb_public` to `lb_public` to align with the naming convention used across the configuration. This improves code clarity and maintainability.

--- data/lb.tf
+++ data/lb.tf
@@ -2,6 +2,6 @@ data "aws_alb_listener" "lb_public_listener" {
   arn = var.meta == "prod" ? data.terraform_remote_state.global.outputs.lb_public_listener_arn_prod : data.terraform_remote_state.global.outputs.lb_public_listener_arn_test
 }
 
-data "aws_alb" "alb_public" {
+data "aws_alb" "lb_public" {
   id = var.meta == "prod" ?  data.terraform_remote_state.global.outputs.lb_public_id_prod : data.terraform_remote_state.global.outputs.lb_public_id_test
 }
