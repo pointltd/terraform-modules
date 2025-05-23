acd4d25dd17757294fe5d09f81bab225ec94d27e
sultonazarmamadazizov <sultonazar.mamadazizov@mail.ru>
2025-05-23 20:53:04 +0100

    change data rds name

--- data/rds.tf
+++ data/rds.tf
@@ -1,3 +1,3 @@
 data "aws_rds_cluster" "partha" {
-    cluster_identifier = var.meta == "prod" ?  data.terraform_remote_state.global.outputs.cluster_identifier_prod : data.terraform_remote_state.global.outputs.cluster_identifier_test
+    cluster_identifier = var.meta == "prod" ?  data.terraform_remote_state.global.outputs.partha_cluster_identifier_prod : data.terraform_remote_state.global.outputs.partha_cluster_identifier_test
 }
